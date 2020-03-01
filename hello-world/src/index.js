2f44030d12fa3732548397f7ce041dd2540d3603
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-01 14:31:22 +0530

    hello world

---
+++ hello-world/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// JSX
+ReactDOM.render(
+  <div>
+    <h1>Hello world</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
