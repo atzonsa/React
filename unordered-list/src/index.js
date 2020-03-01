9f863da72bd5c5a922f35b16638043eb163eac4b
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-01 14:50:00 +0530

    unordered-list

---
+++ unordered-list/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <ul>
+    <li>List Item 1</li>
+    <li>List Item 2</li>
+    <li>List Item 3</li>
+  </ul>,
+  document.getElementById("root")
+);
