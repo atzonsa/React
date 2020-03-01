cbc8746af17a3508759d1c97c496af0c7bdc976f
atzonsa <61501989+atzonsa@users.noreply.github.com>
2020-03-01 15:07:14 +0530

    function profile

---
+++ function-profile/src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Hello my name is Ankit</h1>
+      <p>I'm know how to code in following</p>
+      <ol>
+        <li>c#</li>
+        <li>.net</li>
+        <li>react</li>
+        <li>sql</li>
+        <li>javascript</li>
+        <li>jQery</li>
+        <li>nodejs</li>
+        <li>python</li>
+        <li>java</li>
+        <li>android</li>
+        <li>Typescript etc</li>
+      </ol>
+    </div>
+  );
+}
+
+ReactDOM.render(<MyApp />, document.getElementById("root"));
